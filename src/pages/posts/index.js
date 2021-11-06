5bc97a8ea2c83e44ef857359de8c3a0d283adbd2
Jack Lyons <jack.edward.lyons@gmail.com>
2021-11-06 23:13:54 +1000

    Go back to original post list until deciding on new layout

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import { GatsbyImage } from 'gatsby-plugin-image';
+import tw from 'twin.macro';
 import { Link, graphql } from 'gatsby';
 import { Layout } from '../../components';
-import tw from 'twin.macro';
 
-const PostLink = ({ post, image }) => (
-  <Link to={post.id}>
-    {image}
-
-    <h2 tw="text-center text-xl font-semibold py-2 hover:underline shadow-md cursor-pointer hover:shadow-lg ">
+const PostLink = ({ post }) => (
+  <h2>
+    <Link to={post.id}>
       {post.frontmatter.title} ({post.frontmatter.date})
-    </h2>
-  </Link>
+    </Link>
+  </h2>
 );
 
 const PostPage = ({
@@ -22,32 +19,18 @@ const PostPage = ({
 }) => {
   const Posts = edges
     .filter((edge) => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map((edge) => {
-      const coverImage = edge.node.frontmatter.coverImage;
-      const imgSrc = `images/${coverImage}`;
-      const linkImage = (
-        <img
-          tw="filter hover:blur-sm"
-          src={imgSrc}
-          alt={edge.node.title ?? 'Wildblend image'}
-        />
-      );
-
-      return (
-        <li key={edge.node.id} tw="h-full ">
-          <PostLink post={edge.node} image={linkImage}></PostLink>
-        </li>
-      );
-    });
-
+    .map((edge) => (
+      <li
+        tw="py-2 text-xl hover:underline hover:text-yellow-600"
+        key={edge.node.id}
+      >
+        <PostLink post={edge.node} />
+      </li>
+    ));
   return (
     <Layout>
-      <h1 tw="text-3xl font-semibold underline mt-10 py-4 text-center mb-4">
-        Wildblend posts
-      </h1>
-      <ul className="masonry" tw="grid grid-cols-3 gap-5">
-        {Posts}
-      </ul>
+      <h1 tw="text-2xl underline mt-10 py-4 ">Wildblend posts</h1>
+      <ul tw="list-disc">{Posts}</ul>
     </Layout>
   );
 };
@@ -62,7 +45,6 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
-            coverImage
           }
         }
       }
